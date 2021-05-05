5a4dcae105ec00286dc1d34b5715b077d96897de
DDF <1711356221@qq.com>
2021-05-05 12:08:43 +0800

    Upload files

---
+++ js/footer.js
@@ -0,0 +1,4 @@
+document.writeln("<footer class=\'layout-footer mdl-mega-footer\'>");
+document.writeln("    <div class=\'mdl-mega-footer--middle-section\'>");
+document.writeln("    </div>");
+document.writeln("</footer>");
